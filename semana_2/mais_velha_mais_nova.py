df01b5a5882740d868f64336f24702f1483dcad9
lauramainero <mainerolaura@gmail.com>
2023-07-01 02:23:52 -0300

    update: conditionals

--- semana_2/mais_velha_mais_nova.py
+++ semana_2/mais_velha_mais_nova.py
@@ -2,39 +2,23 @@ idade1 = int(input())
 idade2 = int(input())
 idade3 = int(input())
 idade4 = int(input())
-if idade1 > (idade2 and idade3 and idade4) and idade2 < (idade1 and idade3 and idade4):
-    print("A mais velha tem " + str(idade1))
-    print("A mais nova tem " + str(idade2))
-elif idade1 > (idade2 and idade3 and idade4) and idade3 < (idade1 and idade2 and idade4):
-    print("A mais velha tem " + str(idade1))
-    print("A mais nova tem " + str(idade3))
-elif idade1 > (idade2 and idade3 and idade4) and idade4 < (idade1 and idade2 and idade3):
-    print("A mais velha tem " + str(idade1))
-    print("A mais nova tem " + str(idade4))
-elif idade2 > (idade1 and idade3 and idade4) and idade1 < (idade2 and idade3 and idade4):
-    print("A mais velha tem " + str(idade2))
-    print("A mais nova tem " + str(idade1))
-elif idade2 > (idade1 and idade3 and idade4) and idade3 < (idade1 and idade2 and idade4):
-    print("A mais velha tem " + str(idade2))
-    print("A mais nova tem " + str(idade3))
-elif idade2 > (idade1 and idade3 and idade4) and idade4 < (idade1 and idade2 and idade3):
-    print("A mais velha tem " + str(idade2))
-    print("A mais nova tem " + str(idade4))
-elif idade3 > (idade1 and idade2 and idade4) and idade1 < (idade2 and idade3 and idade4):
-    print("A mais velha tem " + str(idade3))
-    print("A mais nova tem " + str(idade1))
-elif idade3 > (idade1 and idade2 and idade4) and idade2 < (idade1 and idade3 and idade4):
-    print("A mais velha tem " + str(idade3))
-    print("A mais nova tem " + str(idade2))
-elif idade3 > (idade1 and idade2 and idade4) and idade4 < (idade1 and idade2 and idade3):
-    print("A mais velha tem " + str(idade3))
-    print("A mais nova tem " + str(idade4))
-elif idade4 > (idade1 and idade2 and idade3) and idade1 < (idade2 and idade3 and idade4):
-    print("A mais velha tem " + str(idade4))
-    print("A mais nova tem " + str(idade1))
-elif idade4 > (idade1 and idade2 and idade3) and idade2 < (idade1 and idade3 and idade4):
-    print("A mais velha tem " + str(idade4))
-    print("A mais nova tem " + str(idade2))
+
+if idade1 > idade2:
+    maior = idade1
+    menor = idade2
 else:
-    print("A mais velha tem " + str(idade4))
-    print("A mais nova tem " + str(idade3))
+    maior = idade2
+    menor = idade1
+
+if idade3 > maior:
+    maior = idade3
+else:
+    menor = idade3
+
+if idade4 > maior:
+    maior = idade4
+else:
+    menor = idade4
+
+print("A mais velha tem " + str(maior))
+print("A mais nova tem " + str(menor))
